9afca25bc4d60a8e5332ed9ff4b9037138085509
Przem Szkwarek <szkwarek@gmail.com>
2019-05-02 05:23:17 -0700

    added dice icons

--- src/Dice.js
+++ src/Dice.js
@@ -6,11 +6,14 @@ class Dice extends Component {
   render() {
     return <div className="Dice">
       {this.props.dice.map((d, idx) =>
-        <Die handleClick={this.props.handleClick}
+        <Die
+          handleClick={this.props.handleClick}
           val={d}
           locked={this.props.locked[idx]}
           idx={idx}
-          key={idx} />
+          key={idx}
+          disabled={this.props.disabled}
+        />
       )}
     </div>
   }
